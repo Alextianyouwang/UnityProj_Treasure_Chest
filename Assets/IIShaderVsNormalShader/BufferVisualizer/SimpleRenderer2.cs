671a7a2cdc29a06a60401e453d8a0c7be31a2fc1
Alex Tianyou Wang <wangtianyoualex@gmail.com>
2025-02-16 15:41:39 -0800

    AddingMovement_02/16/2025

--- Assets/IIShaderVsNormalShader/BufferVisualizer/SimpleRenderer2.cs
+++ Assets/IIShaderVsNormalShader/BufferVisualizer/SimpleRenderer2.cs
@@ -1,4 +1,8 @@
+using System.Collections;
 using UnityEngine;
+using TriInspector;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
+using System;
 [ExecuteInEditMode]
 public class SimpleRenderer2 : MonoBehaviour
 {
@@ -11,21 +15,73 @@ public class SimpleRenderer2 : MonoBehaviour
     private ComputeBuffer _cb_position;
 
     public int MaxCount = 10000;
+    private int _accum_amount;
+    [Range(0, 1)]
+    public float Lerp;
+
     private void OnEnable()
     {
         _compute_inst = Instantiate(Compute);
+        Initialize();
+
+    }
+    private void Initialize()
+    {
+        _cb_args?.Release();
+        _cb_position?.Release();
         _cb_args = new ComputeBuffer(1, sizeof(uint) * 5, ComputeBufferType.IndirectArguments);
         _cb_position = new ComputeBuffer(MaxCount, sizeof(float) * 3);
         _cb_args.SetData(new uint[] { Mesh.GetIndexCount(0), (uint)MaxCount, Mesh.GetIndexStart(0), Mesh.GetBaseVertex(0), 0 });
         Material.SetBuffer("_PositionBuffer", _cb_position);
-        _compute_inst .SetBuffer(1,"_PositionBuffer", _cb_position);
-        _compute_inst .SetInt("_Dimension", 32);
-        _compute_inst .SetInt("_MaxCount", MaxCount);
-        _compute_inst .SetFloat("_Increment", 100 / 99f);
+        _compute_inst.SetBuffer(1, "_PositionBuffer", _cb_position);
+        _compute_inst.SetInt("_Dimension", 32);
+        _compute_inst.SetInt("_MaxCount", MaxCount);
+        _compute_inst.SetFloat("_Increment", 100 / 99f);
         _compute_inst.SetVector("_Offset", Offset);
-
-
+        _compute_inst.SetFloat("_Lerp", 0);
+        _accum_amount = MaxCount;
     }
+    [Button]
+    public void ShiftIndex()
+    {
+        StartCoroutine(ShiftIndex((int)((UnityEngine.Random.value - 0.5) * 400), 0.5f, null));
+    }
+    [Button]
+    public void RepeatShiftIndex()
+    {
+        StartCoroutine(ShiftIndex((int)((UnityEngine.Random.value - 0.5) * 400), 0.5f, RepeatShiftIndex));
+    }
+    [Button]
+    public void SetShiftIndex(int value)
+    {
+        StartCoroutine(ShiftIndex(value, 0.5f,null));
+    }
+    [Button]
+    public void StopShitIndex()
+    {
+        StopAllCoroutines();
+    }
+    private IEnumerator ShiftIndex(int amount, float duration, Action OnNext) 
+    {
+        float time = 0;
+        _accum_amount += amount;
+        while (time < duration) 
+        {
+            float percentage = time / duration;
+            _compute_inst.SetFloat("_Lerp",  percentage);
+            _compute_inst.SetInt("_IndexOffset", amount);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+        MaxCount = _accum_amount;
+        _compute_inst.SetInt("_MaxCount", _accum_amount);
+
+        Initialize();
+        OnNext?.Invoke();
+    }
+
+
     private void LateUpdate()
     {
         _compute_inst .SetVector("_BotLeft", transform.position);
